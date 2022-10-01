57146b14d5465b287b49f37c84bf7c8f74b5e685
Duy Pham <duypham9895@gmail.com>
2022-10-01 15:10:38 +0700

    Fix returned type

--- utils/fetchPageInfo.ts
+++ utils/fetchPageInfo.ts
@@ -3,5 +3,5 @@ import { PageInfo } from "../types/generated";
 export const fetchPageInfo = async () => {
   const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/pageInfo`);
   const data = await res.json();
-  return data.pageInfo as PageInfo[];
+  return data.pageInfo as PageInfo;
 };
