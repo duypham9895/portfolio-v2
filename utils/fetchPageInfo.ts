b2ca9e37aa2802707efec7fcbb5e7c5c6370f0ff
Duy Pham <duypham9895@gmail.com>
2022-10-01 14:28:10 +0700

    Build util functions to fetch data

---
+++ utils/fetchPageInfo.ts
@@ -0,0 +1,7 @@
+import { PageInfo } from "../types/generated";
+
+export const fetchPageInfo = async () => {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/pageInfo`);
+  const data = await res.json();
+  return data.pageInfo as PageInfo[];
+};
---
+++ utils/fetchSkills.ts
@@ -0,0 +1,7 @@
+import { Skill } from "../types/generated";
+
+export const fetchSkills = async () => {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/skills`);
+  const data = await res.json();
+  return data.skills as Skill[];
+};
