93edbe8698be5a4a2a4954b741c39d41f4c0decf
Duy Pham <duypham9895@gmail.com>
2022-10-02 12:45:55 +0700

    Update variable env into next config

--- next.config.js
+++ next.config.js
@@ -5,6 +5,11 @@ const nextConfig = {
   images: {
     domains: ["cdn.sanity.io"],
   },
+  env: {
+    NEXT_PUBLIC_SANITY_DATASET: "production",
+    NEXT_PUBLIC_SANITY_PROJECT_ID: "z7s4t9k2",
+    NEXT_PUBLIC_BASE_URL: "http://localhost:3000",
+  },
 };
 
 module.exports = nextConfig;
