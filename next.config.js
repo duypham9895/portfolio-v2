98fde4fc5a7b8f57aa3fadff2b08b44a48472a8a
Duy Pham <duypham9895@gmail.com>
2022-10-02 10:39:32 +0700

    Update CDN Sanity into Next config

--- next.config.js
+++ next.config.js
@@ -3,8 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    loader: "cloudinary",
-    path: "https://res.cloudinary.com/duypham9895/image/upload/v1664465677/personal/rcrfw0avep18b2ppuon9.jpg",
+    domains: ["cdn.sanity.io"],
   },
 };
 
