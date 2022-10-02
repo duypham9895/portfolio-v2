8f002dc34a49fc124f749dbe3aaa5015a2a10f8e
Duy Pham <duypham9895@gmail.com>
2022-10-02 11:38:19 +0700

    Fill data into Projects via API

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -1,10 +1,16 @@
 import React from "react";
 import { motion } from "framer-motion";
+import snakeCase from "lodash/snakeCase";
 
-type Props = {};
+import { Project } from "../types/generated";
+import { urlFor } from "../lib/sanity";
+import SanityImageLoader from "./UI/SanityImageLoader";
 
-const Projects = (props: Props) => {
-  const projects = [1];
+type Props = {
+  projects: Project[];
+};
+
+const Projects = ({ projects }: Props) => {
   return (
     <motion.div
       initial={{ opacity: 0 }}
@@ -17,10 +23,10 @@ const Projects = (props: Props) => {
       </h3>
 
       <div className="relative w-full flex overflow-x-scroll overflow-y-hidden snap-x snap-mandatory z-20 scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-[#F7AB0A]/80">
-        {projects.map((id, index) => {
+        {projects.map(({ _id, image, title, summary, technologies }, index) => {
           return (
             <motion.div
-              key={id}
+              key={_id}
               className="w-screen h-screen flex-shrink-0 snap-center flex flex-col space-y-5 items-center justify-center p-20 md:p-44"
             >
               <motion.img
@@ -28,29 +34,34 @@ const Projects = (props: Props) => {
                 transition={{ duration: 1.2 }}
                 whileInView={{ y: 0, opacity: 1 }}
                 viewport={{ once: true }}
-                src="https://res.cloudinary.com/duypham9895/image/upload/v1664464589/personal/mt3tsedtajbkjwg9cpdl.png"
-                alt="personal-portfolio"
+                src={urlFor(image).url()}
+                alt={snakeCase(title)}
                 width="499"
                 height="274"
               />
 
-              <div className="space-y-10 px-0 md:px-10 max-w-6xl">
+              <div className="space-y-5 px-0 md:px-10 max-w-6xl">
                 <h4 className="text-4xl font-semibold text-center">
                   <span className="underline decoration-[#F7AB0A]/50">
                     Case Study {index + 1}/{projects.length}:
                   </span>{" "}
-                  Personal Portfolio
+                  {title}
                 </h4>
-                <p className="text-lg text-center md:text-left">
-                  Lorem, ipsum dolor sit amet consectetur adipisicing elit. Rem
-                  corporis quo quia dolores nisi recusandae nemo deserunt
-                  aliquam laudantium ipsa quis libero, ipsum amet ut et
-                  quibusdam rerum cum! Odit. Lorem ipsum dolor sit amet
-                  consectetur adipisicing elit. Provident hic itaque nam, ad
-                  eaque illum cum! Facere perferendis, delectus illum quas
-                  tempore ipsum esse voluptas? Minima impedit beatae tenetur
-                  dolorem?
-                </p>
+
+                <div className="flex flex-row items-center justify-center mx-auto">
+                  {technologies.map(({ _id, image, title }) => (
+                    <SanityImageLoader
+                      key={_id}
+                      src={image}
+                      className="relative rounded-full"
+                      alt={title}
+                      width="30"
+                      height="30"
+                    />
+                  ))}
+                </div>
+
+                <p className="text-lg text-center md:text-left">{summary}</p>
               </div>
             </motion.div>
           );
